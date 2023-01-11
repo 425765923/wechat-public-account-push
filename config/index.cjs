8f28df52789a810b4a212c93932d80f790a248df
LLL <425765923@qq.com>
2023-01-11 20:05:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -143,7 +143,7 @@ const USER_CONFIG = {
       id: 'o0Tgj539HnkB2Plyxaq2ndZu7QNk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '_XiMlUwh7ylXZtaB6Oqsj98AjOcQ80o9ssoRMOkCwrE',
+      useTemplateId: '4tuXVEF61c5aa-yoAi0rVq-yiAbS-6nzI11ZPcD_nBw',
       // 所在省份或城市，也可以不填
       province: '广东',
       // 所在城市或县区
